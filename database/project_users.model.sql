defbc071d8d769f5dd629016eaa1dfab76f02e90
iFelipe <browicked@gmail.com>
2025-06-24 04:06:43 +0800

    refactor (projectusers) update

--- database/project_users.model.sql
+++ database/project_users.model.sql
@@ -4,11 +4,3 @@ CREATE TABLE IF NOT EXISTS project_users (
     role VARCHAR(50) NOT NULL,
     PRIMARY KEY (project_id, user_id)
 );
-echo "Applying schema from database/project_users.model.sql…\n";
-$sql = file_get_contents('database/project_users.model.sql');
-if ($sql === false) {
-  throw new RuntimeException("Could not read database/project_users.model.sql");
-} else {
-  echo "Creation Success from the database/project_users.model.sql\n";
-}
-$pdo->exec($sql);
