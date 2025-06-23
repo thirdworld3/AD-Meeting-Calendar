1235ad78d1fc87dbe2785eb46ea6bd0d283e5a0a
iFelipe <browicked@gmail.com>
2025-06-24 03:55:54 +0800

    refactor (envsetterutil) update

--- utils/envSetter.util.php
+++ utils/envSetter.util.php
@@ -1,8 +1,10 @@
 <?php
 
-define('BASE_PATH', __DIR__);
+if (!defined('BASE_PATH')) {
+  define('BASE_PATH', dirname(__DIR__));
+}
 
-require_once __DIR__ . '/vendor/autoload.php';
+require_once BASE_PATH . '/vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(BASE_PATH);
 $dotenv->load();
