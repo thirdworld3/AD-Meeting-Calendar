1355fbc484f4bfdcd7ab363b0a958c7ee53097c7
iFelipe <browicked@gmail.com>
2025-06-24 04:05:17 +0800

    refactor (projectsmodel) update

--- database/projects.model.sql
+++ database/projects.model.sql
@@ -5,11 +5,3 @@ CREATE TABLE IF NOT EXISTS projects (
     start_date DATE,
     end_date DATE
 );
-echo "Applying schema from database/projects.model.sql…\n";
-$sql = file_get_contents('database/projects.model.sql');
-if ($sql === false) {
-  throw new RuntimeException("Could not read database/projects.model.sql");
-} else {
-  echo "Creation Success from the database/projects.model.sql\n";
-}
-$pdo->exec($sql);
