956884ada6db4516d66ea2d34bfb1da67abd8498
iFelipe <browicked@gmail.com>
2025-06-24 01:40:04 +0800

    refactor (mongodbchecker) update content

--- handlers/mongodbChecker.handler.php
+++ handlers/mongodbChecker.handler.php
@@ -1,11 +1,15 @@
 <?php
+require_once 'envSetter.util.php';
+
+$uri = $_ENV['MONGO_URI'];
+
 try {
-    $mongo = new MongoDB\Driver\Manager("mongodb://host.docker.internal:27111");
+    $mongo = new MongoDB\Driver\Manager($uri);
 
     $command = new MongoDB\Driver\Command(["ping" => 1]);
     $mongo->executeCommand("admin", $command);
 
-    echo "✅ Connected to MongoDB successfully.  <br>";
+    echo "✅ Connected to MongoDB successfully.<br>";
 } catch (MongoDB\Driver\Exception\Exception $e) {
-    echo "❌ MongoDB connection failed: " . $e->getMessage() . "  <br>";
+    echo "❌ MongoDB connection failed: " . $e->getMessage() . "<br>";
 }
